78174a808ca7af21ba27bac9e54ecc68c672a187
Pedro Caninas <pgcaninas@gmail.com>
2022-10-14 21:25:45 -0300

    construção dos grafos e print matriz de adjacência

--- Algoritmos Grafos/Matriz e Lista de adjacência.py
+++ Algoritmos Grafos/Matriz e Lista de adjacência.py
@@ -1,41 +1,65 @@
-arq_grafo = open("IC\Algoritmos Grafos\grafo.txt", "r")
+def montar_grafo(arquivo):
+    vertices = []
+    grafo = dict()
+    passado = -1
+    i = -1
 
-linhas = arq_grafo.readlines()
-
-dic = dict()
-matriz = []
-passado = -1
-vertices = []
-i = -1
-
-for linha in linhas:
-    linha = linha.split(",")
-    vertice_a = linha[0]
-    vertice_b = linha[1].strip("\n")
-
-    if vertice_a != passado:
-        dic[vertice_a] = []
-        i += 1
-        dic[vertice_a].append(vertice_b)
-        # matriz.append([])
-        # matriz[i].append(vertice_b)
-    else:
-        dic[vertice_a].append(vertice_b)
-        #matriz[i].append(vertice_b)
-    passado = vertice_a
-
-print(dic , vertices)
-
-for vertice in vertices:
-    print("   ", vertice, end="")
-
-print("\n", end="")
-for vertice in vertices:
-    print(matriz[vertices.index(vertice)])
-
-    print(vertice, "  X", "\n")
+    linhas = arq_grafo.readlines()
     
-#for i in matriz:
+    for linha in linhas:
+        linha = linha.split(",")
+        vertice_a = linha[0]
+        vertice_b = linha[1].strip("\n")
+
+        if vertice_a in grafo.keys():
+            grafo[vertice_a].append(vertice_b)
+        else:
+            vertices.append(vertice_a)
+            grafo[vertice_a] = [vertice_b]
+
+    vertices.sort()
+    print(grafo, vertices)
+    return grafo, vertices
 
 
+def printar_matriz(grafo, vertices_ordem):  
+    matriz = []
 
+    print("   ", end="")
+    for vertice in vertices_ordem:  # primeira linha
+        print(vertice, end="  ")
+        matriz.append(grafo[vertice])
+    print("")
+
+    for linha in vertices_ordem:    #resto das linhas
+        print(linha, end="  ")
+        for coluna in vertices_ordem:
+            print(f"{grafo[linha].count(coluna)}  ", end="")
+        print("")
+
+
+arq_grafo = open("Algoritmos Grafos\grafo.txt", "r")
+
+grafo, vertices = montar_grafo(arq_grafo)
+printar_matriz(grafo, vertices)
+
+arq_grafo.close()
+
+def tem_caminho(g_inicio, g_final, i):  #arrumar
+    if len(grafo[g_inicio]) == i+1:
+        i = 0
+
+    print("g_inicio:", g_inicio, "indice:", i)
+    print(grafo[g_inicio][i])
+
+    g_inicio = grafo[g_inicio][i]
+
+    if g_inicio == g_final:
+        return True
+
+    i += 1
+    return tem_caminho(g_inicio, g_final, i)
+
+
+i = 0
+print(tem_caminho("1","9",i))
