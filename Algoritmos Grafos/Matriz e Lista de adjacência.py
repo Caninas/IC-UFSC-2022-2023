14302f1fda9bf8c36ec4dec421148b03656b9486
Pedro Caninas <pgcaninas@gmail.com>
2022-10-27 21:08:36 -0300

    implementação inicial do tempo

--- Algoritmos Grafos/Matriz e Lista de adjacência.py
+++ Algoritmos Grafos/Matriz e Lista de adjacência.py
@@ -1,3 +1,6 @@
+from random import randint
+import math
+
 class Grafo:
     def __init__(self, arquivo):
         self.vertices = []
@@ -9,6 +12,18 @@ class Grafo:
         
         self.montar_grafo()
 
+        self.t = 0
+
+        self.S = 1000
+        self.I = 10
+        self.R = 0
+        
+        self.X = randint(1, 30)
+
+        self.taxa_virulencia = 0.55
+        self.taxa_recuperaçao = 0.1
+        #self.taxa_distanciamento = 0
+
     def montar_grafo(self):
         linhas = self.arquivo.readlines()
         
@@ -16,15 +31,14 @@ class Grafo:
             linha = linha.split(",")
             vertice_a = linha[0]
             vertice_b = linha[1].strip("\n")
-
             if vertice_a in self.grafo.keys():
                 self.grafo[vertice_a].append(vertice_b)
-                if vertice_b not in self.grafo.keys():
-                    self.vertices.append(vertice_b)
-                    self.grafo[vertice_b] = []
             else:
                 self.vertices.append(vertice_a)
                 self.grafo[vertice_a] = [vertice_b]
+                if vertice_b not in self.grafo.keys():
+                    self.vertices.append(vertice_b)
+                    self.grafo[vertice_b] = []
 
         self.vertices.sort()
 
@@ -57,6 +71,22 @@ class Grafo:
 
                 self.tem_caminho(g_inicio, g_final)
 
+    def avançar_t(self, t):
+        for i in range(0, t):
+            e = int(self.taxa_recuperaçao * self.I)
+            v = int(self.taxa_virulencia * self.X)
+            self.R = self.R + e
+            print(f"Contatos Adequados: {self.X} \nNovos Infectados: {v}")
+            self.I = self.I - e + v
+            self.S = self.S - v
+            self.X = randint(1, int(0.2*self.S))
+            self.info()
+            print("")
+        
+    def info(self):
+        print(f"I: {(self.I)} \nS: {(self.S)} \nR: {(self.R)}")
+        #print(f"Valores Reais:\nI: {self.I} \nS: {self.S} \nR: {self.R}")
+        print(f"Total: {round(self.I) + round(self.S) + round(self.R)}")
 
 
 arq_grafo = "Algoritmos Grafos/grafo.txt"
@@ -64,5 +94,6 @@ arq_grafo = "Algoritmos Grafos/grafo.txt"
 grafo = Grafo(arq_grafo)
 grafo.printar_matriz()
 print(grafo.grafo)
-grafo.tem_caminho("2", "2")
-print(grafo.caminho)
+grafo.info()
+grafo.avançar_t(50)
+#grafo.info()
