7a38280fb2e40e595ecf10bdf2ebc65a28b0c0ed
Rasengam1000 <pgcaninas@gmail.com>
2023-03-09 16:16:38 -0300

    reorganização de pastas, renomeação de variáveis para legibilidade e novas adjacências

--- Instancia RJ/Instancia_RJ.py
+++ Instancia RJ/Instancia_RJ.py
@@ -20,12 +20,12 @@ class Modelo:
         self.arquivo = open(arq_final, "r", encoding="utf-8")
 
         # tempo atual
-        self.t = 0
+        self.t = 1
 
         # variaveis para a construçao do grafico posteriormente
         self.tempos = []
         self.SIRs = []
-
+        self.pico_infectados = 0
 
         # variaveis globais, se aplicam a todos os vértices
         # retiradas da pagina 8 e 9 do artigo do modelo
@@ -38,16 +38,16 @@ class Modelo:
         self.alpha = 2/5        # fraçao de pessoas que respeitam o distanciamento, fator de distanciamento
                                 # aplicado por vertice ou globalmente
 
-        self.lambdaS = 2/5       # fraçao de pessoas que respeitam distanciamento mas precisam sair (podem ser infectados)
-        self.lambdaI = 1/10      
-        self.lambdaR = 3/5
+        self.lambda_S = 2/5       # fraçao de pessoas que respeitam distanciamento mas precisam sair (podem ser infectados)
+        self.lambda_I = 1/10      
+        self.lambda_R = 3/5
 
         # diz se o SIR que não respeita distanciamento esta ou nao em seu vertice original
         self.juntos = True
-        self.grafo = self.GerarGrafo()
+        self.grafo = self.gerar_grafo()
 
 
-    def GerarGrafo(self):
+    def gerar_grafo(self):
         g = nx.Graph()
 
         for linha in self.arquivo:
@@ -80,21 +80,21 @@ class Modelo:
 
         return g
 
-    def PrintarGrafo(self):
+    def printar_grafo(self):
         plt.figure(figsize=(10,8))
 
-        nx.draw(self.grafo, with_labels=True, font_weight='bold', font_size=6, node_size=500, clip_on=True)
+        nx.draw(self.grafo, with_labels=True, font_weight='bold', font_size=6, node_size=200, clip_on=True)
 
         plt.show()
 
-    def PrintarEstadoVertice(self, vertice):
+    def printar_estado_vertice(self, vertice):
         print(f"{vertice}: {self.grafo.nodes[vertice]}")
     
-    def PrintarGraficoSIRxT(self):
+    def printar_grafico_SIRxT(self):
         fig = plt.figure(1)
         ax = fig.add_subplot(111)
 
-        plt.xlim(left=0, right=self.t-1)
+        plt.xlim(left=self.tempos[0], right=self.t-1)
         plt.gca().set_prop_cycle('color', ['red', '#55eb3b', 'blue'])
         plt.plot(self.tempos, self.SIRs)
         plt.gca().get_yaxis().get_major_formatter().set_scientific(False)
@@ -109,40 +109,42 @@ class Modelo:
         #     nome, atributos = node
 
         #     for vizinho in self.grafo.edges(nome):
-        #         nodevizinho = self.grafo.nodes[vizinho[1]]["SIRddd"][nome]
-        #         atributos["SIRdd"]["S"] += nodevizinho["S"]
-        #         atributos["SIRdd"]["I"] +=  nodevizinho["I"]
-        #         atributos["SIRdd"]["R"] += nodevizinho["R"]
+        #         node_vizinho = self.grafo.nodes[vizinho[1]]["SIRddd"][nome]
+        #         atributos["SIRdd"]["S"] += node_vizinho["S"]
+        #         atributos["SIRdd"]["I"] +=  node_vizinho["I"]
+        #         atributos["SIRdd"]["R"] += node_vizinho["R"]
         #         self.grafo.nodes[vizinho[1]]["SIRddd"][nome] = {}
 
         # print console todos os vertices
         # for vertice in self.grafo.nodes():
-        #     self.PrintarEstadoVertice(vertice)
+        #     self.printar_estado_vertice(vertice)
 
         plt.show()
 
 
-    def AvançarTempo(self, t):
-        # prob y** pi -> i = prob y* pi (nao respeitam e ficam é igual ao respeitam (realizada sobre lambdaS*))
-        i = 0
+    def avançar_tempo(self, t):
+        # prob y** pi -> i = prob y* pi (nao respeitam e ficam é igual ao respeitam (realizada sobre lambda_S*))
         if self.t == 0 or self.juntos == True:                                             # distribuiçao inicial de pessoas
             self.juntos = False
             for node in list(self.grafo.nodes(data=True)):
                 nome, atributos = node
 
-                S2pontossaindo = floor(atributos["SIRdd"]["S"] * atributos["beta"])
-                I2pontossaindo = floor(atributos["SIRdd"]["I"] * atributos["beta"])
-                R2pontossaindo = floor(atributos["SIRdd"]["R"] * atributos["beta"])
+                S_2pontos_saindo = floor(atributos["SIRdd"]["S"] * atributos["beta"])
+                I_2pontos_saindo = floor(atributos["SIRdd"]["I"] * atributos["beta"])
+                R_2pontos_saindo = floor(atributos["SIRdd"]["R"] * atributos["beta"])
 
-                atributos["SIRdd"]["S"] -= S2pontossaindo * len(self.grafo.edges(nome))
-                atributos["SIRdd"]["I"] -= I2pontossaindo * len(self.grafo.edges(nome))
-                atributos["SIRdd"]["R"] -= R2pontossaindo * len(self.grafo.edges(nome))
+                atributos["SIRdd"]["S"] -= S_2pontos_saindo * len(self.grafo.edges(nome))
+                atributos["SIRdd"]["I"] -= I_2pontos_saindo * len(self.grafo.edges(nome))
+                atributos["SIRdd"]["R"] -= R_2pontos_saindo * len(self.grafo.edges(nome))
 
                 for vizinho in self.grafo.edges(nome):
-                    self.grafo.nodes[vizinho[1]]["SIRddd"][nome] = {"S": S2pontossaindo, "I": I2pontossaindo, "R": R2pontossaindo}
+                    self.grafo.nodes[vizinho[1]]["SIRddd"][nome] = {"S": S_2pontos_saindo, "I": I_2pontos_saindo, "R": R_2pontos_saindo}
 
         for tempo in range(t):
-            self.tempos.append(tempo)
+            print("tempo=", self.t)
+
+            self.tempos.append(tempo+1)
+
             for node in list(self.grafo.nodes(data=True)):
                 # x = tempo
                 # y = [S, I, R]
@@ -157,35 +159,35 @@ class Modelo:
 
 
             self.SIRs.append([])
-            somaSIR = [0, 0, 0]
+            soma_SIR = [0, 0, 0]
 
             for node in list(self.grafo.nodes(data=True)):
                 nome, atributos = node
 
                 # guardando valores SIR para usar no grafico
-                somaSIR[0] += atributos["SIRd"]["S"] + atributos["SIRdd"]["S"] + atributos["SIRdddantes"]["S"]
-                somaSIR[1] += atributos["SIRd"]["I"] + atributos["SIRdd"]["I"] + atributos["SIRdddantes"]["I"]
-                somaSIR[2] += atributos["SIRd"]["R"] + atributos["SIRdd"]["R"] + atributos["SIRdddantes"]["R"]
+                soma_SIR[0] += atributos["SIRd"]["S"] + atributos["SIRdd"]["S"] + atributos["SIRdddantes"]["S"]
+                soma_SIR[1] += atributos["SIRd"]["I"] + atributos["SIRdd"]["I"] + atributos["SIRdddantes"]["I"]
+                soma_SIR[2] += atributos["SIRd"]["R"] + atributos["SIRdd"]["R"] + atributos["SIRdddantes"]["R"]
                 
-                #! Calculo Xponto + Sdd que fica (Ypi = Ypi -> i)
+                #! Calculo X_ponto + Sdd que fica (Ypi = Ypi -> i)
 
-                Nponto = floor(self.lambdaS * atributos["SIRd"]["S"]) + floor(self.lambdaI * atributos["SIRd"]["I"]) + floor(self.lambdaR * atributos["SIRd"]["R"])
+                Nponto = floor(self.lambda_S * atributos["SIRd"]["S"]) + floor(self.lambda_I * atributos["SIRd"]["I"]) + floor(self.lambda_R * atributos["SIRd"]["R"])
                 N2pontos = atributos["SIRdd"]["S"] + atributos["SIRdd"]["I"] + atributos["SIRdd"]["R"]
                 N3pontos = atributos["SIRdddantes"]["S"] + atributos["SIRdddantes"]["I"] + atributos["SIRdddantes"]["R"]
 
                 # prob de acontecer um encontro de Sd / Sdd com Infectados no geral (Ypi = Ypi -> i)
-                Yponto = (floor(self.lambdaI * atributos["SIRd"]["I"]) + atributos["SIRdd"]["I"] + atributos["SIRdddantes"]["I"]) / ((Nponto + N2pontos + N3pontos) - 1)
+                Y_ponto = (floor(self.lambda_I * atributos["SIRd"]["I"]) + atributos["SIRdd"]["I"] + atributos["SIRdddantes"]["I"]) / ((Nponto + N2pontos + N3pontos) - 1)
                 
-                Xponto = 0          # numero de encontros de S com I no vertice i
-                X2pontosii = 0
+                X_ponto = 0          # numero de encontros de S com I no vertice i
+                X_2pontos_ii = 0
 
                 # range S bonito 1 ponto
-                for i in range(floor(self.lambdaS * atributos["SIRd"]["S"])):    # calculo do numero de encontros na pop de suscetiveis que respeitam mas tem q sair
-                    Xponto += random() < Yponto                                 # baseado na probabilidade de Yponto
+                for i in range(floor(self.lambda_S * atributos["SIRd"]["S"])):    # calculo do numero de encontros na pop de suscetiveis que respeitam mas tem q sair
+                    X_ponto += random() < Y_ponto                                 # baseado na probabilidade de Y_ponto
 
-                # X2pontos = X2pontosii + X2pontosij (somatorio Y2pontosii(=Yponto) + somario dos que vieram dos vizinhos e, portanto, usam a probabilidade Y deste vertice)
+                # X_2pontos = X_2pontos_ii + X_2pontos_ij (somatorio Y_2pontos_ii(=Y_ponto) + somatorio dos que vieram dos vizinhos e, portanto, usam a probabilidade Y deste vertice)
                 for i in range(atributos["SIRdd"]["S"]):                    # calculo do numero de encontros na pop de suscetiveis que nao respeitam e restam no vertice
-                    X2pontosii += random() < Yponto                         # baseado na probabilidade de Yponto
+                    X_2pontos_ii += random() < Y_ponto                      # baseado na probabilidade de Y_ponto
 
 
                 # Cada grupo do SIR (respeitam, nao respeitam, vizinhos) tem probabilidades unicas (2 loops acima)
@@ -193,52 +195,78 @@ class Modelo:
                 # prob de acontecer um encontro de Sddd (estrangeiros) com Infectados nesse vertice
                 # (do ponto de vista do vertice vizinho = Ypi -> j = Ypj, ou seja, do ponto de vista desse vertice = Ypi)
                 for vizinho in atributos["SIRddd"].values():                # SIR t+1 vizinhos
-                    X2pontosij = 0
+                    X_2pontos_ij = 0
 
                     for i in range(vizinho["S"]):       # calculo do numero de encontros na pop de suscetiveis que vem de outros vertices
-                        X2pontosij += random() < Yponto                                            # baseado na probabilidade de Y2pontos
+                        X_2pontos_ij += random() < Y_ponto                                            # baseado na probabilidade de Y_2pontos
                         
-                    recuperados_novosddd = ceil(self.e * vizinho["I"])
+                    recuperados_novos_ddd = ceil(self.e * vizinho["I"])
 
-                    vizinho["S"] = vizinho["S"] - floor(self.v * X2pontosij)
-                    vizinho["I"] = vizinho["I"] - recuperados_novosddd + floor(self.v * X2pontosij)
-                    vizinho["R"] = vizinho["R"] + recuperados_novosddd
+                    vizinho["S"] = vizinho["S"] - floor(self.v * X_2pontos_ij)
+                    vizinho["I"] = vizinho["I"] - recuperados_novos_ddd + floor(self.v * X_2pontos_ij)
+                    vizinho["R"] = vizinho["R"] + recuperados_novos_ddd
         
                 # SIR t+1 SIRponto e SIR2pontos
-                recuperados_novosd = ceil(self.e * atributos["SIRd"]["I"])
-                recuperados_novosdd = ceil(self.e * atributos["SIRdd"]["I"])
-                
-                if (nome == "Bangu"):
-                    i += 1
-                    print("tempo=",self.t)
-                    print(recuperados_novosd, recuperados_novosdd, floor(self.v * Xponto))
+                recuperados_novos_d = ceil(self.e * atributos["SIRd"]["I"])
+                recuperados_novos_dd = ceil(self.e * atributos["SIRdd"]["I"])
 
-                atributos["SIRd"]["S"] = atributos["SIRd"]["S"] - floor(self.v * Xponto)              # X = quantidade total de encontros, v*X pessoas suscetiveis de fato infectadas
-                atributos["SIRdd"]["S"] = atributos["SIRdd"]["S"] - floor(self.v * X2pontosii)
+                atributos["SIRd"]["S"] = atributos["SIRd"]["S"] - floor(self.v * X_ponto)              # X = quantidade total de encontros, v*X pessoas suscetiveis de fato infectadas
+                atributos["SIRdd"]["S"] = atributos["SIRdd"]["S"] - floor(self.v * X_2pontos_ii)
 
-                atributos["SIRd"]["I"] = atributos["SIRd"]["I"] - recuperados_novosd + floor(self.v * Xponto)
-                atributos["SIRdd"]["I"] = atributos["SIRdd"]["I"] - recuperados_novosdd + floor(self.v * X2pontosii)
+                atributos["SIRd"]["I"] = atributos["SIRd"]["I"] - recuperados_novos_d + floor(self.v * X_ponto)
+                atributos["SIRdd"]["I"] = atributos["SIRdd"]["I"] - recuperados_novos_dd + floor(self.v * X_2pontos_ii)
 
-                atributos["SIRd"]["R"] = atributos["SIRd"]["R"] + recuperados_novosd
-                atributos["SIRdd"]["R"] = atributos["SIRdd"]["R"] + recuperados_novosdd
+                atributos["SIRd"]["R"] = atributos["SIRd"]["R"] + recuperados_novos_d
+                atributos["SIRdd"]["R"] = atributos["SIRdd"]["R"] + recuperados_novos_dd
 
-            self.SIRs[tempo] = [somaSIR[0], somaSIR[1], somaSIR[2]]
+            self.SIRs[tempo] = [soma_SIR[0], soma_SIR[1], soma_SIR[2]]
+
+            if not any(i[1] > soma_SIR[1] for i in self.SIRs):
+                self.pico_infectados = soma_SIR[1]
+
             self.t += 1
 
 
-os.chdir(r"C:\Users\rasen\Documents\Programação\IC Iniciação Científica\Instancia RJ")
+#os.chdir(r"C:\Users\rasen\Documents\Programação\IC Iniciação Científica\Instancia RJ")
+os.chdir(r"D:\Programação\IC Iniciação Científica\Instancia RJ")
 
-adjacencias = "./txts/adjacencias.txt"
-nomes = "./txts/bairros apenas.txt"
-populaçao = "./tabelas/Tabela pop por idade e grupos de idade (2973).xls"
-arquivo_final = "./txts/arquivo_final.txt"
+# "./txts/normal (real)/adjacencias.txt"
+# "./txts/normal (real)/arquivo_final.txt"
+# "./txts/otimizado/adjacencias.txt"
+arquivo_adjacencias = "./txts/zona sul/adjacencias_zona_sul.txt"
+arquivo_final = "./txts/zona sul/arquivo_final.txt"
+arquivo_ID_nomes = "./txts/relaçao ID - bairros.txt"
+tabela_populaçao = "./tabelas/Tabela pop por idade e grupos de idade (2973).xls"
 
-# txt = Txt(adjacencias, nomes, arquivo_final, populaçao)
-# txt.gerar_arquivo_destino()
+# arquivo_pico_infectados = "./txts/pico_infectados.txt"
+# pico_infec = open(arquivo_pico_infectados, "w")
+
+#txt = Txt(arquivo_adjacencias, arquivo_ID_nomes, arquivo_final, tabela_populaçao)
+#txt.gerar_arquivo_destino()
 
 
 m = Modelo(arquivo_final)
-m.GerarGrafo()
-m.AvançarTempo(200)
-m.PrintarGraficoSIRxT()
-#m.PrintarGrafo()
+m.gerar_grafo()
+#print(m.grafo.nodes())
+m.avançar_tempo(200)
+
+m.printar_grafico_SIRxT()
+
+#m.printar_grafo()
+
+
+# pico_infec.write(f"Normal\n")
+# for i in range(50):
+#     m = Modelo(arquivo_final)
+#     m.gerar_grafo()
+#     m.avançar_tempo(75)
+#     pico_infec.write(f"{str(m.pico_infectados)}\n")
+
+# pico_infec.write(f"\nOtimizado\n")
+# arquivo_final = "./txts/otimizado/arquivo_final.txt"
+
+# for i in range(50):
+#     m = Modelo(arquivo_final)
+#     m.gerar_grafo()
+#     m.avançar_tempo(75)
+#     pico_infec.write(f"{str(m.pico_infectados)}\n")
--- Instancia RJ/Txt.py
+++ Instancia RJ/Txt.py
@@ -1,30 +1,75 @@
 import xlrd
+import os
+
 
 class Txt:            # modela os dados no arquivo final (que sera lido)
     def __init__(self, adjacencias, nomes, destino, populaçao):
         self.adjacencias = open(adjacencias, "r", encoding="utf-8")
         self.nomes = open(nomes, "r", encoding="utf-8")
         self.destino = open(destino, "w", encoding="utf-8")
-        self.populaçao = xlrd.open_workbook(populaçao).sheet_by_index(1)
+        self.tabela_populaçao = xlrd.open_workbook(populaçao).sheet_by_index(1) # index 1 - tabela 2010
+
+        self.nome_bairros_por_ID = {}
 
     def close(self):
         self.adjacencias.close()
         self.nomes.close()
         self.destino.close()
 
-    def leitura_populaçao(self, nome_bairros):
-        populaçao_dict = {}
+    def leitura_populaçao(self):
+        populaçao = {}
 
-        for rx in range(7, self.populaçao.nrows):                            # ler populaçao
-            nome = str(self.populaçao.cell(rx,0).value).strip()
+        for rx in range(7, self.tabela_populaçao.nrows):                            # ler populaçao
+            nome = str(self.tabela_populaçao.cell(rx,0).value).strip()
 
-            if nome in nome_bairros.values():
-                populaçao_dict[nome] = int(self.populaçao.cell(rx,1).value)
+            if nome in self.nome_bairros_por_ID.values():
+                populaçao[nome] = int(self.tabela_populaçao.cell(rx,1).value)
         
-        return populaçao_dict
+        return populaçao
 
     def gerar_arquivo_destino(self):
-        nome_bairros = {}
+        self.nome_bairros_por_ID = {}
+
+        for linha in self.nomes:                # salvar relação ID - bairros
+            linha = linha.strip()
+
+            if linha != "":
+                num, bairro = linha.split(" ", maxsplit=1)
+                num = int(num)
+
+                self.nome_bairros_por_ID[num] = bairro
+        
+        cidades_ja_visitadas = set()
+        linhasTextoFinal = []
+
+        populaçao = self.leitura_populaçao()   # leitura da tabela de população
+        
+        for linha in self.adjacencias:                   # ler adjacencias e montar string final
+            linha = linha.strip()
+            
+            if linha != "":
+                num_bairro = int(linha.split(" ", maxsplit=1)[0])
+                nome_bairro = self.nome_bairros_por_ID[num_bairro]
+
+                adjs = {self.nome_bairros_por_ID[int(x)] for x in linha.split(" ")[1:]}
+
+                linhasTextoFinal.append(f"{nome_bairro}" + f"{', ' if len(adjs) else ''}" +
+                    f"{', '.join(adjs)}/{num_bairro}, {populaçao[nome_bairro]}, {1/(len(adjs) + 1)}\n")
+
+        
+        self.destino.writelines(linhasTextoFinal)
+        self.close()
+
+
+        # for num_bairro, adj in adjs.items():      verifica a consistencia das adjacencias
+        #     for bairro2 in adj:                   aresta xy e yx devem existir
+        #         if bairro2 not in adj:
+        #             #print("not")
+        #             adjs[num_bairro_nome[bairro2]].add(self.nome_bairros_por_ID[num_bairro])
+
+
+    def gerar_arquivo_destino_wesley(self):
+        self.nome_bairros_por_ID = {}
 
         for linha in self.nomes:
             linha = linha.strip()
@@ -33,36 +78,112 @@ class Txt:            # modela os dados no arquivo final (que sera lido)
                 num, bairro = linha.split(" ", maxsplit=1)
                 num = int(num)
 
-                nome_bairros[num] = bairro
-        
-        cidades_ja_visitadas = set()
-        linhas_texto = []
+                if num >= 13:
+                    num -= 1
 
-        populaçao = self.leitura_populaçao(nome_bairros)
+                self.nome_bairros_por_ID[num-1] = bairro
+        
+        N = len(self.nome_bairros_por_ID.keys()) # numero de vertices
+        M = 0                                 # numero de arestas
 
         adjs = {}
-        num_bairro_nome = {}
-
+        txt_adjs = ""
+        
         for linha in self.adjacencias:                   # ler adjacencias e ontar string final
             linha = linha.strip()
             
             if linha != "":
                 num_bairro = int(linha.split(" ", maxsplit=1)[0])
-                num_bairro_nome[nome_bairros[num_bairro]] = num_bairro
-                adjs[num_bairro] = {nome_bairros[int(x)] for x in linha.split(" ")[1:]}
+                #num_bairro_nome[self.nome_bairros_por_ID[num_bairro]] = num_bairro
+                adjs[num_bairro] = [str(int(x)) for x in linha.split(" ")[1:]]
+                for adj in adjs[num_bairro]:
+                    txt_adjs += f"{num_bairro} {adj} {1/(len(adjs[num_bairro]) + 1)}\n"
+        
+        print(adjs)
+        for num, adj in adjs.items():
+            for num2 in adj:
+                if not str(num) in adjs[int(num2)]:
+                    print(num, num2)
 
-                # adjs = [nome_bairros[int(x)] for x in linha.split(" ")[1:]]
+        for num, adj in adjs.items():
+            M += len(adj)
 
-                # linhas_texto.append(f"{nome_bairros[num_bairro]}" + f"{', ' if len(adjs) else ''}" +
-                #     f"{', '.join(adjs)}/{num_bairro}, {populaçao[nome_bairros[num_bairro]]}, {1/(len(adjs) + 1)}\n")
+        self.destino.write(f"{N} {M}\n")            # primeira linha
 
-        for num_bairro, adj in adjs.items():
-            for bairro2 in adj:
-                adjs[num_bairro_nome[bairro2]].add(nome_bairros[num_bairro])
+        self.populaçao = self.leitura_populaçao()
 
-        for num_bairro, adj in adjs.items():
-            linhas_texto.append(f"{nome_bairros[num_bairro]}" + f"{', ' if len(adj) else ''}" +
-                    f"{', '.join(adj)}/{num_bairro}, {populaçao[nome_bairros[num_bairro]]}, {1/(len(adj) + 1)}\n")
+        for i in range(len(self.nome_bairros_por_ID)):
+            self.destino.write(f"{self.populaçao[self.nome_bairros_por_ID[i]]}" + f"{' ' if i != N-1 else ''}")
 
-        self.destino.writelines(linhas_texto)
-        self.close()
+        self.destino.write("\n")
+
+        for i in range(len(self.nome_bairros_por_ID)):    
+            self.destino.write("0" + f"{' ' if i != N-1 else ''}")
+
+        self.destino.write("\n")
+
+        for i in range(len(self.nome_bairros_por_ID)):    
+            self.destino.write("0" + f"{' ' if i != N-1 else ''}")
+
+        self.destino.write("\n")
+
+        self.destino.writelines(txt_adjs)
+        a = open("./adja.txt", "w", encoding="utf-8")
+        txt_a = ""
+    
+
+        print(adjs)
+        for num, adj in adjs.items():
+            txt_a += f"{num}" + f"{' ' if len(adj) else ''}" + " ".join(adj) + "\n"
+
+        a.writelines(txt_a)
+
+    def ajuste_arquivo_adjacencias(self):
+        adjs = {}
+
+        self.nome_bairros_por_ID = {}
+
+        for linha in self.nomes:                # salvar relação ID - bairros
+            linha = linha.strip()
+
+            if linha != "":
+                num, bairro = linha.split(" ", maxsplit=1)
+                num = int(num)
+
+                self.nome_bairros_por_ID[num] = bairro
+
+        num_bairro_nome = {}
+
+        for linha in self.adjacencias:                   # ler adjacencias e montar string final
+            linha = linha.strip()
+            
+            if linha != "":
+                num_bairro = int(linha.split(" ", maxsplit=1)[0])
+                nome_bairro = self.nome_bairros_por_ID[num_bairro]
+
+                num_bairro_nome[nome_bairro] = num_bairro
+                adjs[num_bairro] = {str(x) for x in linha.split(" ")[1:]}
+
+
+        for num_bairro, adj in adjs.items():      # verifica a consistencia das adjacencias
+            for bairro2 in adj:                   # aresta xy e yx devem existir
+                if str(num_bairro) not in adjs[int(bairro2)]:
+                    print(num_bairro, bairro2, "not")
+                    adjs[int(bairro2)].add(str(num_bairro))
+        
+        adj = open("adj novo.txt", "w", encoding="utf-8")
+
+        for i in range(160):
+            adj.write(f"{i+1} {' '.join(adjs[i+1])}\n")
+
+# os.chdir(r"D:\Programação\IC Iniciação Científica\Instancia RJ")
+
+# # "./txts/normal (real)/adjacencias.txt"
+# # "./txts/otimizado/adjacencias.txt"
+# arquivo_adjacencias = "./txts/normal (real)/adjacencias.txt"
+# arquivo_destino = "./txts/normal (real)/arquivo_final.txt"
+# arquivo_ID_nomes = "./txts/relaçao ID - bairros.txt"
+# tabela_populaçao = "./tabelas/Tabela pop por idade e grupos de idade (2973).xls"
+
+# t = Txt(arquivo_adjacencias, arquivo_ID_nomes, arquivo_destino, tabela_populaçao)
+# t.gerar_arquivo_destino()
