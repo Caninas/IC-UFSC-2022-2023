cf4844ac903a9ed6259afa0e840402754258d1f6
Caninas <pgcaninas@gmail.com>
2023-11-01 17:18:49 -0300

    busca em largura e printar grafo

--- SBPO - apresentacao/Exemplos.py
+++ SBPO - apresentacao/Exemplos.py
@@ -1,4 +1,5 @@
 import networkx as nx
+from networkx.drawing.nx_pydot import graphviz_layout
 import matplotlib
 import matplotlib.pyplot as plt
 from ast import literal_eval as make_tuple
@@ -17,28 +18,41 @@ class Exemplos:
             adj, coords = linha.split("/", maxsplit=1)
             bairro, *adjs = adj.split(", ")
 
-            adjs = [(bairro, adj, {"pintado": False}) for adj in adjs]
+            adjs = [(bairro, adj, {"color": "black"}) for adj in adjs]
             self.grafo.add_node(bairro,
                                 coord=make_tuple(coords), 
             )
             self.grafo.add_edges_from(adjs)
 
     def buscaProfundidade(self, inicio):
-        return
         visitados = set(inicio)
+        pilha = [inicio]
+        anterior = dict()
+        vertice_anterior = None
 
-        for vizinho in self.grafo_original.edges(inicio):
-            vizinho = vizinho[1]
-            if vizinho not in visitados:
-                anterior[vizinho] = v
-                self.busca_em_profundidade(vizinho, anterior, visitados, niveis, nivel+1)
+        while len(pilha):
+            vertice = pilha[-1]
+            pilha.pop()
+
+            visitados.add(vertice)
+
+            if vertice_anterior:
+                anterior[vertice] = vertice_anterior
+            vertice_anterior = vertice
+
+            for vizinho in self.grafo.edges(vertice):
+                vizinho = vizinho[1]
+            
+                if vizinho not in visitados:
+                    pilha.append(vizinho)    
+
+        return anterior
 
 
     def buscaLargura(self, inicio):
-        fila = []
+        fila = [inicio]
         visitados = {inicio}
-        anterior = {}
-        fila.append(inicio)
+        anterior = dict()
 
         while len(fila):
             v = fila.pop(0)
@@ -52,28 +66,33 @@ class Exemplos:
 
         return anterior
     
-    def gerarArvore(self, tipo):
+    def gerarArvore(self, tipo, inicio):
         arvore = dict()
         if tipo == "profundidade":
-            arvore = self.buscaProfundidade()
+            arvore = self.buscaProfundidade(inicio)
         elif tipo == "largura":
-            arvore = self.buscaLargura()
+            arvore = self.buscaLargura(inicio)
+
+        self.grafo.remove_edges_from(e for e in self.grafo.edges if e not in nx.Graph([(a, b) for a,b in arvore.items()]).edges)
 
     def pintarAresta(self, aresta):
         pass
 
-    def printarGrafo(self):
+    def printarGrafo(self, arvore=False):
 
         pos = dict()
 
-        #pos = nx.spring_layout(nx.Graph(self.grafo))
 
+        pos = nx.spring_layout(nx.Graph(self.grafo))
+        
+        #pos = graphviz_layout(self.grafo, prog="dot")
         for vertice in self.grafo.nodes(data=True):
             pos[vertice[0]] = vertice[1]["coord"]
-      
-        g = self.grafo
+        
 
-        nx.draw(g, pos, with_labels=True, font_weight='bold', font_size=11, node_size=300) #fonte 6 nodesize 200
+        colors = nx.get_edge_attributes(self.grafo, 'color').values()
+        print(colors)
+        nx.draw(self.grafo, pos, edge_color=colors, with_labels=True, font_weight='bold', font_size=11, node_size=300,) #fonte 6 nodesize 200
         print(pos)
         #plt.savefig(fr"Exemplo grafo.png", format="png", dpi=300)
 
@@ -82,6 +101,7 @@ class Exemplos:
 
 a = Exemplos("./Grafos/SP.txt")
 
-print(a.grafo.nodes(data=True))
-print(a.grafo.edges(data=True))
+# print(a.grafo.nodes(data=True))
+# print(a.grafo.edges(data=True))
+a.gerarArvore("profundidade", "Liberdade")
 a.printarGrafo()
